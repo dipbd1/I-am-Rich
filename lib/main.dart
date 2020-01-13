61179809abfdfb5f2e8c67bac523142a2c0dfea7
Dip <dipbd1@gmail.com>
2020-01-13 15:43:30 +0600

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey[700],
+        appBar: AppBar(
+          title: Center(
+            child: Text(
+              "I Am Rich",
+            ),
+          ),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage("images/diamond.png")
+          ),
+        ),
+      ),
+    ),
+  );
+}
